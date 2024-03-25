04d120cfa4108105be5ec94f8d9d951b5cd5ae25
YaakovG <ygoldenbergyo@gmail.com>
2024-03-25 10:49:48 -0400

    it works now

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hey what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("whats your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("Whats your favorite band?");
+              string band = Console.ReadLine();
+
+
+               Console.WriteLine($"{name} was walking past a {color} store and he saw on the\n" +
+               $"street all set up {band}, then a {animal} walked past, that was fun!");
+
+
+
+
         }
+
+
+
+
     }
-}
+};
